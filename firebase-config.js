2a42bb73f9421c0ddf456a30fe5290d70921274f
TecInfo.com <iranildo.tecnologia@outlook.com>
2026-05-20 01:58:32 -0300

    Refactor Firebase config to use Firestore and Storage

--- firebase-config.js
+++ firebase-config.js
@@ -1,37 +1,48 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getDatabase, ref, onValue, set, update, remove, push, get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+    getFirestore, collection, doc, getDocs, getDoc, setDoc, updateDoc, 
+    deleteDoc, query, where, orderBy, addDoc, onSnapshot, arrayUnion, 
+    arrayRemove, writeBatch, Timestamp 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { 
+    getStorage, ref, uploadBytes, getDownloadURL, deleteObject 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+import { 
+    getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword,
+    signOut, onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// Configuração oficial do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDBHaHN0WWnesZtH8Z76n5RmTsy3Sn8EBI",
-    authDomain: "tutubebidasapp-28b77.firebaseapp.com",
-    databaseURL: "https://tutubebidasapp-28b77-default-rtdb.firebaseio.com",
-    projectId: "tutubebidasapp-28b77",
-    storageBucket: "tutubebidasapp-28b77.firebasestorage.app",
-    messagingSenderId: "92833412828",
-    appId: "1:92833412828:web:cbeb1ba840bfcbbd686619"
+    apiKey: "AIzaSyCaS5J1mbJtxfZsA6O6WGpCBKKJimG27sw",
+    authDomain: "popfood-ea5ff.firebaseapp.com",
+    projectId: "popfood-ea5ff",
+    storageBucket: "popfood-ea5ff.firebasestorage.app",
+    messagingSenderId: "677810682230",
+    appId: "1:677810682230:web:96bbf1b452edf9fcfb6257"
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 const auth = getAuth(app);
 
-// Exportar tudo que será usado nos outros arquivos
+// Nomes das coleções
+const COLLECTIONS = {
+    PRODUCTS: 'products',
+    CATEGORIES: 'categories',
+    COMPLEMENTS: 'complements',
+    ORDERS: 'orders',
+    CLIENTS: 'clients',
+    RESTAURANT_PROFILE: 'restaurantProfile',
+    CURRENT_SESSION: 'currentSession'
+};
+
 export { 
-    db, 
-    auth, 
-    ref, 
-    onValue, 
-    set, 
-    update, 
-    remove, 
-    push, 
-    get,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut
+    db, storage, auth, 
+    collection, doc, getDocs, getDoc, setDoc, updateDoc, 
+    deleteDoc, query, where, orderBy, addDoc, onSnapshot, 
+    arrayUnion, arrayRemove, writeBatch, Timestamp,
+    COLLECTIONS
 };
